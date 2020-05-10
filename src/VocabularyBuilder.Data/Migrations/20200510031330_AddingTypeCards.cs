edccd5cd4c752148187d25a7af35370893cb578e
ever1509 <ever1509@gmail.com>
2020-05-09 21:34:43 -0600

    Adding data to Db in order to handle typecards and categories inside of a flashcard

---
+++ src/VocabularyBuilder.Data/Migrations/20200510031330_AddingTypeCards.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VocabularyBuilder.Data.Migrations
+{
+    public partial class AddingTypeCards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [dbo].[TypeCards] ([Description]) VALUES ('Daily')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[TypeCards] ([Description]) VALUES ('Weekly')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[TypeCards] ([Description]) VALUES ('Monthly')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from [dbo].[TypeCards]");
+        }
+    }
+}
---
+++ src/VocabularyBuilder.Data/Migrations/20200510032504_AddingCategories.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VocabularyBuilder.Data.Migrations
+{
+    public partial class AddingCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Animals')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Body Parts')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Clothes')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Education')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Family')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Food')");
+            migrationBuilder.Sql("INSERT INTO [dbo].[Categories] ([Description]) VALUES('Business')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from [dbo].[Categories]");
+        }
+    }
+}
